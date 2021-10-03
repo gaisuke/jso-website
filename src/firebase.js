2b1ad17fd3fb649e5359352857084c84e956a8b6
Dani Munif <danimunif@cs.unej.ac.id>
2021-10-03 23:34:23 +0700

    done integrating firebase and done tested fetching data

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: `${process.env.REACT_APP_FIREBASE_KEY}`,
+  authDomain: "jso-backend.firebaseapp.com",
+  projectId: "jso-backend",
+  storageBucket: "jso-backend.appspot.com",
+  messagingSenderId: "93171705306",
+  appId: "1:93171705306:web:646f9ea580aea75e044ce3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default getFirestore();
